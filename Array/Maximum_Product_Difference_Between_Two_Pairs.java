b1195f41f510943721b8bacfe90e249176eb272e
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2021-09-07 12:24:53 +0530

    Update Maximum_Product_Difference_Between_Two_Pairs.java

--- Array/Maximum_Product_Difference_Between_Two_Pairs.java
+++ Array/Maximum_Product_Difference_Between_Two_Pairs.java
@@ -1,36 +1,23 @@
 //https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
 class Solution {
     public int maxProductDifference(int[] nums) {
-        int max1, max2;
-        max1 = max2 = Integer.MIN_VALUE;
-        int min1, min2;
-        min1 = min2 = Integer.MAX_VALUE;
+        int max1=Integer.MIN_VALUE, max2=Integer.MIN_VALUE;
+        int min1=Integer.MAX_VALUE, min2=Integer.MAX_VALUE;
         int n = nums.length;
         
         for(int i: nums) {
-            if(i > max1) {
+            if(i >= max1) {
                 max2 = max1;
                 max1 = i;
-            } else if(i == max1) {
+            } else if(i < max1 && i > max2)
                 max2 = i;
-                max1 = i;
-            } else if(i < max1 && i > max2) {
-                max2 = i;
-            }
             
-            if(i < min1) {
+            if(i <= min1) {
                 min2 = min1;
                 min1 = i;
-            } else if(i == min1) {
-                min1 = i;
+            } else if(i > min1 && i < min2)
                 min2 = i;
-            } else if(i > min1 && i < min2) {
-                min2 = i;
-            }
         }
-        
-        System.out.println(max1 + " " + max2);
-        System.out.println(min1 + " " + min2);
         
         return (max1*max2)-(min1*min2);
     }
