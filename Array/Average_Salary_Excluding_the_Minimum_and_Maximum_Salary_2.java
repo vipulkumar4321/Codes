9b34638d6538bbabfe0c480c3e8bb6f8a9d14669
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2021-10-10 05:06:08 +0530

    Create Average_Salary_Excluding_the_Minimum_and_Maximum_Salary_2.java

---
+++ Array/Average_Salary_Excluding_the_Minimum_and_Maximum_Salary_2.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+class Solution {
+    public double average(int[] salary) {
+        double ans=0, sum=0;
+        int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE, n=salary.length;
+        
+        for(int i=0; i<n; i++) {
+            sum += salary[i];
+            if(max < salary[i]) {
+                sum -= salary[i];
+                sum += max;
+                max = salary[i];
+            }
+            
+            if(min > salary[i]) {
+                sum -= salary[i];
+                sum += min;
+                min = salary[i];
+                if(i == 0)
+                    sum +=1;
+            }            
+        }
+        
+        ans = sum/(n-2);
+        return ans;
+    }
+}
