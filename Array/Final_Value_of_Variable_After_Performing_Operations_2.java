57c76e63c31e86cb49a34b96d9dcc4a0cd6b4cc6
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2021-11-02 23:20:22 +0530

    Create Final_Value_of_Variable_After_Performing_Operations_2.java

---
+++ Array/Final_Value_of_Variable_After_Performing_Operations_2.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        
+        for(int i=0; i<operations.length; i++) {
+            if(operations[i].charAt(1) == '+') 
+                x++;
+            else 
+                x--;
+        }
+        
+        return x;
+    }
+}
