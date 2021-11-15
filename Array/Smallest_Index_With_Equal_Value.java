56690fc11a9ee99be0e1527f29f5a6f4da0eb17b
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2021-11-15 12:14:44 +0530

    Create Smallest_Index_With_Equal_Value.java

---
+++ Array/Smallest_Index_With_Equal_Value.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/smallest-index-with-equal-value/submissions/
+class Solution {
+    public int smallestEqual(int[] nums) {               
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == (i%10)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
