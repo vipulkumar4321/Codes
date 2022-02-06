560a675d83aae4447fde32843af5d102788ecbe2
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2022-02-06 13:28:15 +0530

    Create Factorial_Trailing_Zeroes.java

---
+++ Maths/Factorial_Trailing_Zeroes.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/factorial-trailing-zeroes/
+class Solution {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        
+        for(int i=5; i<=n; i=i*5) {
+            res = res + n/i;
+        }
+        
+        return res;
+    }
+}
