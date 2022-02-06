4bf2ce7ef691f880bf16c26a6b964d9f2f8041e4
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2022-02-06 13:06:31 +0530

    Create Palindrome_Number.java

---
+++ Maths/Palindrome_Number.java
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/palindrome-number/submissions/
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) {
+            return false;
+        }
+        
+        int rev=0;
+        int temp = x;
+        
+        while(temp > 0) {
+            rev = rev * 10 + (temp%10);
+            temp = temp/10;
+        }
+        
+        if(x == rev) {
+            return true;
+        }
+        
+        return false;
+    }
+}
